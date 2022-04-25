7f446ecd16e2364e04dc4fd054a44375ef3a5200
jeonghoHub <tjsrsdqq4@gmail.com>
2022-04-25 23:16:13 +0900

    정규식 학습

---
+++ src/main/java/study/StringCalculator.java
@@ -0,0 +1,8 @@
+package study;
+
+public class StringCalculator {
+    public int add() {
+
+        return 10;
+    }
+}
---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,51 @@
+package study;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.stream;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+//    + * ?
+    @ParameterizedTest
+    @CsvSource(value = "22 + 3 * 4 / 2")
+    void 주어진_문자열에서_연산자와_피연산자_추출(String value) {
+        String[] operand = value.replaceAll("[\\s]","").replaceAll("^\\d", " ").split(" ");
+        String[] operator = value.replaceAll("[\\s\\d]", "").split("");
+
+
+//        System.out.println(operand);
+        System.out.println(Arrays.toString(operand));
+        System.out.println(Arrays.toString(operator));
+
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = "2 + 3 * 4 / 2:10", delimiter = ':')
+    void add(String value, int separator) {
+        int result = stringCalculator.add();
+        assertThat(result).isEqualTo(separator);
+    }
+
+
+
+
+    @AfterEach
+    void reSet() {
+        stringCalculator = null;
+    }
+
+}
