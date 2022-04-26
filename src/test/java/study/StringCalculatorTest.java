1de2ae2967f37c49cceee17a7f88b30fd751ec0b
jeonghoHub <tjsrsdqq4@gmail.com>
2022-04-26 23:54:11 +0900

    계산기 작성

--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -22,21 +22,19 @@ public class StringCalculatorTest {
     @ParameterizedTest
     @CsvSource(value = "22 + 3 * 4 / 2")
     void 주어진_문자열에서_연산자와_피연산자_추출(String value) {
-        String[] operand = value.replaceAll("[\\s]","").replaceAll("^\\d", " ").split(" ");
+        String[] operand = value.replaceAll("[\\s]","").replaceAll("[^\\d]", " ").split(" ");
         String[] operator = value.replaceAll("[\\s\\d]", "").split("");
 
-
-//        System.out.println(operand);
-        System.out.println(Arrays.toString(operand));
-        System.out.println(Arrays.toString(operator));
+        assertThat("[22,3,4,2]").isEqualTo(operand);
+        assertThat("[+,*,/]").isEqualTo(operator);
 
     }
 
 
     @ParameterizedTest
-    @CsvSource(value = "2 + 3 * 4 / 2:10", delimiter = ':')
+    @CsvSource(value = {"2 + 3 * 4 / 2:10", "30+2 / 2 + 5 - 10 *2:22"}, delimiter = ':')
     void add(String value, int separator) {
-        int result = stringCalculator.add();
+        double result = stringCalculator.add(value);
         assertThat(result).isEqualTo(separator);
     }
 
